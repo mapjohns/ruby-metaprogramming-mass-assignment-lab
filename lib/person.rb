7b37d9c221d551b4fb5884de2585151882fb56de
Matthew <mattj20@gmail.com>
2022-06-03 09:51:53 -0700

    commit tree eb072c1317ed940562421ac446336f5057d29e22

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
+require 'pry'
 class Person
   #your code here
+  def initialize(hash_brown)
+    hash_brown.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
 end
